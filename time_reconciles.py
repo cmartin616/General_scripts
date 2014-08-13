0525074b35b815fde32d72e92a013ea221408803
cmartin616 <cmartin616@gmail.com>
2014-08-13 09:35:04 -0500

    Initial commit
    A short script used to time ESRI ParcelFabric reconciles in order to troubleshoot slow response times.

---
+++ time_reconciles.py
@@ -0,0 +1,19 @@
+import time
+
+version = raw_input("Enter the version number: ")
+raw_input('Press enter to start.   ')
+tic = time.time()
+raw_input('Press enter to stop.   ')
+toc = time.time()
+s = toc-tic
+
+m, s = divmod(s, 60)
+h, m = divmod(m, 60)
+timeFormat = "%d:%02d:%02d" % (h, m, s)
+
+
+reconcileFile = r'c:/users/**/desktop/rec_time.txt'
+f = open(reconcileFile, 'a')
+output = [version, ' - ', str(timeFormat), ' to reconcile.\n']
+f.write(''.join(output))
+f.close
